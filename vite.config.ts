8bc9b35933e893d960bbb0588519ee0ccf91db6d
Kathak Dabhi <kathakdabhi@gmail.com>
2026-07-12 14:16:57 +0530

    fix: update base path in Vite configuration to root

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/ailnd-website/',
+  base: '/',
   plugins: [tailwindcss(), react()],
 })
